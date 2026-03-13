d75c9284fcbdf2a09958d19c2686f2f1af2abfeb
Arthur <arthurtrindadebat@gmail.com>
2026-03-12 22:00:00 -0300

    V1.01

--- marketplace-showcase/src/app/pages/home/home.ts
+++ marketplace-showcase/src/app/pages/home/home.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
-const API_BASE = 'http://localhost:3001';
+const API_BASE = 'https://worknexdeploy-production.up.railway.app:3001';
 
 @Component({
   selector: 'app-home',
